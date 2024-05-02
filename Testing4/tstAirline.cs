f4a1c6e0981af58a76b2aa2cb380c11f2ddcd709
Justin Doll <P2684916@my365.dmu.ac.uk>
2024-05-02 12:12:42 +0100

    Added testing for the airline class

---
+++ Testing4/tstAirline.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstAirline
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsAirline AnAirline = new clsAirline();
+            //test to see that it exists
+            Assert.IsNotNull(AnAirline);
+        }
+    }
+}
