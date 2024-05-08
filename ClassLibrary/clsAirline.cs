ce62dc92234ce7d7547a8fb88a329a91f830df10
Justin Doll <P2684916@my365.dmu.ac.uk>
2024-05-08 11:02:05 +0100

    Added Testing to each attribute

--- ClassLibrary/clsAirline.cs
+++ ClassLibrary/clsAirline.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsAirline
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int AirlineID { get; set; }
+        public text Name { get; set; }
+        public string AirlineName { get; set; }
+        public string AirlineEmail { get; set; }
+        public int AirlinePhoneNumber { get; set; }
     }
 }
