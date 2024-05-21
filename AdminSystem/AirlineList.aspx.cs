7bb7af153f908953d3570df2eeb652f08941714d
Justin Doll <P2684916@my365.dmu.ac.uk>
2024-05-21 14:28:50 +0100

    Finished Practical 12

--- AdminSystem/AirlineList.aspx.cs
+++ AdminSystem/AirlineList.aspx.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //if this is the first time the page is displayed 
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayAirlines();
+        }
     }
-}
+
+    void DisplayAirlines()
+    {
+        //create an instance of the airline collection
+        clsAirlineCollection Airlines = new clsAirlineCollection();
+        //set the data source to list of airlines in the collection
+        lstAirlineList.DataSource = Airlines.AirlineList;
+        //set the name of the primary key
+        lstAirlineList.DataValueField = "AirlineEmail";
+        //set the data field to display
+        lstAirlineList.DataTextField = "AirlineName";
+        //bind the data to the list
+        lstAirlineList.DataBind();
+      }
+    }
---
+++ ClassLibrary/clsAirlineCollection.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsAirlineCollection
+    {
+        //private data member for the list
+        List<clsAirline> mAirlineList = new List<clsAirline>();
+
+        //public property for the address list
+        public List<clsAirline> AirlineList
+        {
+            get
+            {
+                //return the private data
+                return mAirlineList;
+            }
+            set
+            {
+                //set the private data
+                mAirlineList = value;
+            }
+        }
+
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mAirlineList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public clsAirline ThisAirline { get; set; }
+
+
+        //constructor for the class
+        public clsAirlineCollection()
+        {
+            //variable for the index
+            Int32 Index = 0;
+            //variable to store the record count
+            Int32 RecordCount = 0;
+            //object for the data connect
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblAirline_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount) 
+            { 
+                //create a blank Airline
+                clsAirline AnAirline = new clsAirline();
+                //read in the fields for the current record
+                AnAirline.WiFi = Convert.ToBoolean(DB.DataTable.Rows[Index]["HasWi-Fi"]);
+                AnAirline.AirlineID = Convert.ToInt32(DB.DataTable.Rows[Index]["AirlineID"]);
+                AnAirline.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["RegistrationDate"]);
+                AnAirline.AirlineName = Convert.ToString(DB.DataTable.Rows[Index]["AirlineName"]);
+                AnAirline.AirlineEmail = Convert.ToString(DB.DataTable.Rows[Index]["AirlineEmail"]);
+                AnAirline.AirlinePhoneNumber = Convert.ToInt32(DB.DataTable.Rows[Index]["AirlinePhoneNumber"]);
+                //add the record to the private data member
+                mAirlineList.Add(AnAirline);
+                //point at the next record
+                Index++;
+            }
+
+            //create the items of test data
+            clsAirline TestItem = new clsAirline();
+            //set its properties
+            TestItem.WiFi = true;
+            TestItem.AirlineID = 1;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.AirlineName = "Blissful Travels";
+            TestItem.AirlineEmail = "BlissfulTravels@gmail.com";
+            TestItem.AirlinePhoneNumber = 75643576;
+            //add the test item to the test list
+            mAirlineList.Add(TestItem);
+            //re initialise the object for some new data
+            TestItem = new clsAirline();
+            //set its properties
+            TestItem.WiFi = true;
+            TestItem.AirlineID = 2;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.AirlineName = "Blissful_Travels";
+            TestItem.AirlineEmail = "Bliss@gmail.com";
+            TestItem.AirlinePhoneNumber = 73053015;
+            //add the item to the test list
+            mAirlineList.Add(TestItem);
+        }
+
+        }
+    }
+
